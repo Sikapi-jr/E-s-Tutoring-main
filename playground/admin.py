0b69063f7dd96d47674d19dcc698a90d686f6b2e
Elvis Sikapi <elvissikapi@gmail.com>
2025-05-14 12:47:52 -0400

    Initial Dockerized Django setup

--- playground/admin.py
+++ playground/admin.py
@@ -13,8 +13,6 @@ from .models import (
     AcceptedTutor,
     Hours,
     WeeklyHours,
-    AiChatSession,
-    AiRequest,
 )
 
 # If you want to customize admin interface per model, you can define ModelAdmin classes here.
@@ -32,5 +30,3 @@ admin.site.register(TutorResponse)
 admin.site.register(AcceptedTutor)
 admin.site.register(Hours)
 admin.site.register(WeeklyHours)
-admin.site.register(AiChatSession)
-admin.site.register(AiRequest)
