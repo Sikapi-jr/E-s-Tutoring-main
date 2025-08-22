09412e07245d402faa6f155743388a81401dbe91
Elvis Sikapi <elvs.sikapi@ontariotechu.net>
2025-08-22 01:55:56 -0400

    fix

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -419,7 +419,7 @@ export default function Home() {
         </div>
 
         {/* MIDDLE COL */}
-        <div style={{ width: "55%", padding: "1rem 0", marginTop: "25vh" }} className="home-middle-column">
+        <div style={{ width: "55%", padding: "1rem 0", paddingTop: "25rem" }} className="home-middle-column">
           <div
             className="table-wrapper"
             style={{
